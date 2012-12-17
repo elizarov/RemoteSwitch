6e1d0dcb6d2b6c7671da9828b26735a009f684a0
elizarov <elizarov@gmail.com>
2012-12-17 10:45:38 +0400

    Minor changes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 RemoteSwtich
 ============
 
-Arduino code for remote switching of 433 MHz controlled Duwi sockets (or others based on SM5172 3^12 decoder)
+Arduino code for remote switching of 433 MHz controlled Duewi sockets (or others based on SM5172 3^12 decoder).
--- blink_led.cpp
+++ blink_led.cpp
@@ -1,9 +1,24 @@
+/*
+    Copyright (C) 2012 Roman Elizarov
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <Arduino.h>
 #include <Metro.h>
 
 #include "blink_led.h"
-
-#define  BLINK_LED_PIN 13
 
 Metro blinkLedTime(1000, true);
 boolean blinkLedState = false;
@@ -11,8 +26,6 @@ boolean blinkLedState = false;
 void blinkLed() {
   if (blinkLedTime.check()) {
     blinkLedState = !blinkLedState;
-    digitalWrite(BLINK_LED_PIN, blinkLedState);
+    digitalWrite(LED_BUILTIN, blinkLedState);
   }
 }
-
-
